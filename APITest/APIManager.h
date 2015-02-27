f0d40737d1e03dc7595736c0e3752266c7925ce7
jbhadra15 <jennifer.bhadra@gmail.com>
2015-02-26 21:29:17 -0800

    Remove un-necessary header comments

--- APITest/APIManager.h
+++ APITest/APIManager.h
@@ -1,10 +1,4 @@
-//
-//  APIManager.h
-//  APITest
-//
-//  Created by Jen Patten on 2/8/15.
-//  Copyright (c) 2015 Jen. All rights reserved.
-//
+
 
 #import <Foundation/Foundation.h>
 
--- APITest/APIManager.m
+++ APITest/APIManager.m
@@ -1,10 +1,4 @@
-//
-//  APIManager.m
-//  APITest
-//
-//  Created by Jen Patten on 2/8/15.
-//  Copyright (c) 2015 Jen. All rights reserved.
-//
+
 
 #import "APIManager.h"
 #import "NSURLRequest+OAuth.h"
